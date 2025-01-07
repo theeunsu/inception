29679f587aa1346fb67461ae95843a722593c017
theeunsu <theeunsu@gmail.com>
2025-01-07 22:36:56 +0100

    mariadb init

--- srcs/requirements/mariadb/tools/mariadb_init.sh
+++ srcs/requirements/mariadb/tools/mariadb_init.sh
@@ -20,3 +20,24 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	echo "Starting temporary MariaDB server for configuration..."
 	mysqld_safe --skip-networking &
 	sleep 5
+
+	echo "Configuring MariaDB..."
+	mysql -u root <<-EOSQL
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+		DELETE FROM mysql.user WHERE USER='';
+		DROP DATABASE IF EXISTS test; 
+		CREATE DATABASE ${DB_NAME};
+		CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+		GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+		FLUSH PRIVILEGES;
+	EOSQL
+
+	echo "Shutting down temporary MariaDB server..."
+	mysqladmin -u root -p"${DB_ROOT_PASSWORD}" shutdown
+else
+	echo "MariaDB data directory found. Skipping initialization..."
+fi
+
+# Start MariaDB server in foreground mode
+echo "Starting MariaDB server..."
+exec mysqld_safe
